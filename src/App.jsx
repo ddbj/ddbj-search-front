c1824831805d5a5f59561d46bafe3b22fc6560eb
Satoshi Onoda <satoshionoda@gmail.com>
2024-02-21 15:00:16 +0900

    WIP スタイリング設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Page from "./components/parts/Page/index.jsx";
 import Search from "./components/features/Search/index.jsx";
 import {AppIntlProvider} from "./providers/AppIntlProvider.jsx";
+import Layout from "./components/parts/Layout/index.jsx";
+import './styles/globals.scss';
 
 function App() {
 
   return (
     <>
       <AppIntlProvider>
+        <Layout>
         <Page>
           <Search />
         </Page>
+        </Layout>
       </AppIntlProvider>
     </>
   )
