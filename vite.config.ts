301cb16cf0dbc2fc2751a12de65f21177b7517ad
Satoshi Onoda <satoshionoda@gmail.com>
2024-02-22 17:40:53 +0900

    WIP PNPM+TypeScriptに移行

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,10 +8,10 @@ export default defineConfig({
     alias: [
       {
         find: /^~.+/,
-        replacement: (val) => {
+        replacement: (val:string) => {
           return val.replace(/^~/, "");
         },
-      },
+      } as any,
     ],
   },
 })
