ed5907f60c977b2298d0e7368d77b6c3a826e07f
suecharo <suehiro619@gmail.com>
2024-04-23 19:00:57 +0900

    Fix import order

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+import fs from "fs";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
-import fs from "fs";
 
 const insideDockerContainer = fs.existsSync("/.dockerenv")
 
