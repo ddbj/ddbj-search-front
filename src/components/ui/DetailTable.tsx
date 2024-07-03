56f25e6da7abe03ac7c9fee362c0036aed0447db
Satoshi Onoda <satoshionoda@gmail.com>
2024-07-03 21:56:24 +0900

    xml の prefetch を本番サーバーに限定

--- src/components/ui/DetailTable.tsx
+++ src/components/ui/DetailTable.tsx
@@ -76,13 +76,20 @@ const PrefetchedDownloadLinks = (props: {
   const [isActive, setActive] = React.useState(true);
   useEffect(() => {
     if (!https) return;
-    fetch(https, { method: "HEAD" }).then((res) => {
-      if (res.ok) {
-        setActive(true);
-      } else {
-        setActive(false);
-      }
-    });
+    const targetHost = new URL(https).host;
+    const currentHost = window.location.host;
+    if (targetHost === currentHost) {
+      fetch(https, { method: "HEAD" }).then((res) => {
+        if (res.ok) {
+          setActive(true);
+        } else {
+          setActive(false);
+        }
+      });
+    } else {
+      setActive(true);
+      console.warn("target host is different from current host. So prefetch is skipped.");
+    }
   }, [https]);
   return (
     <p className={"flex gap-x-2"}>
