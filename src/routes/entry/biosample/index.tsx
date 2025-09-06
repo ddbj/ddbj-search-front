53be71c79cce8cd1c6bcfa5a3ced1da2759e72e4
satoshionoda <satoshionoda@yohak.design>
2025-09-06 22:54:39 +0900

    Refactor biosample route to integrate API fetching.
    Replaced dummy response with real API fetching using react-query. Updated validation schema and added loader dependencies to properly handle query data. Improved structure to align with biosample-specific requirements.

--- src/routes/entry/biosample/index.tsx
+++ src/routes/entry/biosample/index.tsx
@@ -1,23 +1,39 @@
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { zodValidator } from "@tanstack/zod-adapter";
-import { dummyResponse } from "@/consts/api.ts";
+import { API_PATH_ALL_ENTRIES_LIST } from "@/api/paths.ts";
 import { dbTypes } from "@/consts/db.ts";
 import { useUpdateSearchFunctions } from "@/features/searchResult/hooks/useUpdateSearchFunctions.ts";
+import { fetchBioSamples } from "@/fetch/fetchBioSampleEntries.ts";
 import { SearchResultLayout } from "@/layout/SearchResultLayout.tsx";
-import { baseSearchSchema } from "@/schema/search/base.ts";
+import { biosampleSearchSchema } from "@/schema/search/bioSample.ts";
+import type { AnySearchParams } from "@/schema/search/any.ts";
 import type { ComponentProps } from "react";
+
+const makeQuery = (params: AnySearchParams) => {
+  return queryOptions({
+    queryKey: [API_PATH_ALL_ENTRIES_LIST, ...Object.entries(params)],
+    queryFn: () => fetchBioSamples(params),
+  });
+};
 
 export const Route = createFileRoute("/entry/biosample/")({
   component: PageComponent,
-  validateSearch: zodValidator(baseSearchSchema),
+  validateSearch: zodValidator(biosampleSearchSchema),
+  loaderDeps: ({ search }) => ({ ...search }),
+  loader: async ({ context, deps }) => await context.queryClient.ensureQueryData(makeQuery(deps)),
 });
 
 function PageComponent() {
+  const search = Route.useSearch();
+  const query = makeQuery(search);
+  const { data } = useSuspenseQuery(query);
+
   const props = {
     entryType: dbTypes.biosample,
     params: Route.useSearch(),
     updateFunctions: useUpdateSearchFunctions(),
-    data: dummyResponse,
+    data,
   } satisfies ComponentProps<typeof SearchResultLayout>;
   return <SearchResultLayout {...props} />;
 }
