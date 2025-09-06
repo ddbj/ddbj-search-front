6e531cbb9a9512b14715e654c3a66c2c694a0a4a
satoshionoda <satoshionoda@yohak.design>
2025-09-06 22:50:42 +0900

    Add mock handlers for entry and BioProject list APIs
    Introduced `getAllEntryList` and `getBioProjectList` handlers using MSW to mock the respective API endpoints. These handlers generate paginated response data with placeholder entries for testing purposes.

---
+++ src/msw/handlers/entries/getAllEntryList.ts
@@ -0,0 +1,32 @@
+import { http, HttpResponse } from "msw";
+import { v4 as uuidv4 } from "uuid";
+import { API_PATH_ALL_ENTRIES_LIST } from "@/api/paths.ts";
+import type { EntryListResponse } from "@/api/entries/base.ts";
+
+export const getAllEntryList = http.get<
+  {},
+  {},
+  EntryListResponse,
+  typeof API_PATH_ALL_ENTRIES_LIST
+>(API_PATH_ALL_ENTRIES_LIST, async ({ request }) => {
+  // await delay(Math.random() * 500 + 300);
+  const url = new URL(request.url);
+  const search = url.searchParams;
+  const page: number = parseInt(search.get("page") ?? "1");
+  return HttpResponse.json({
+    page,
+    perPage: 10,
+    total: 10000,
+    items: Array(10)
+      .fill(0)
+      .map((_, i) => {
+        return {
+          identifier: uuidv4().slice(0, 8),
+          type: "bioproject",
+          title: "Draparnaldia sp. CCAC 6921, genomic data.",
+          dbXrefs: { "sra-study": 1, "sra-run": 18, "sra-experiment": 4, biosample: 1 },
+          datePublished: "2013-05-31",
+        };
+      }),
+  });
+});
