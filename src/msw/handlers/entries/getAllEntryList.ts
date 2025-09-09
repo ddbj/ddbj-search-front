635a73cab0ce1dd13156101121065df4cdfacbd6
satoshionoda <satoshionoda@yohak.design>
2025-09-09 13:41:20 +0900

    Add type filtering and randomization in getAllEntryList handler

--- src/msw/handlers/entries/getAllEntryList.ts
+++ src/msw/handlers/entries/getAllEntryList.ts
@@ -1,6 +1,8 @@
 import { http, HttpResponse } from "msw";
 import { v4 as uuidv4 } from "uuid";
 import { API_PATH_ALL_ENTRIES_LIST } from "@/api/paths.ts";
+import { dbTypeList } from "@/consts/db.ts";
+import { randomPick } from "@/utils/randomPick.ts";
 import type { EntryListResponse } from "@/api/entries/base.ts";
 
 export const getAllEntryList = http.get<
@@ -13,6 +15,7 @@ export const getAllEntryList = http.get<
   const url = new URL(request.url);
   const search = url.searchParams;
   const page: number = parseInt(search.get("page") ?? "1");
+  const types = (search.get("types") ?? dbTypeList.join(",")).split(",");
   return HttpResponse.json({
     page,
     perPage: 10,
@@ -23,7 +26,7 @@ export const getAllEntryList = http.get<
       .map((_, _i) => {
         return {
           identifier: uuidv4().slice(0, 8),
-          type: "bioproject",
+          type: randomPick(types),
           title: "Draparnaldia sp. CCAC 6921, genomic data.",
           dbXrefs: { "sra-study": 1, "sra-run": 18, "sra-experiment": 4, biosample: 1 },
           datePublished: "2013-05-31",
