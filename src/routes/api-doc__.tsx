954c5e54c8d192dfc28f29c80284d914d3068dba
satoshionoda <satoshionoda@yohak.design>
2026-02-04 11:57:43 +0900

    fix: tanstack error

--- src/routes/api-doc__.tsx
+++ src/routes/api-doc__.tsx
@@ -2,6 +2,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import { ApiPage } from "@/layout/ApiPage.tsx";
 import "@scalar/api-reference-react/style.css";
 
-export const Route = createFileRoute("/__api-doc")({
+export const Route = createFileRoute("/api-doc__")({
   component: ApiPage,
 });
