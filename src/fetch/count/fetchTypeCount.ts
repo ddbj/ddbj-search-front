08ed41aca946c51b40b20f03cbaf4030d08d1aea
satoshionoda <satoshionoda@yohak.design>
2026-02-13 09:46:10 +0900

    WIP

--- src/fetch/count/fetchTypeCount.ts
+++ src/fetch/count/fetchTypeCount.ts
@@ -1,19 +1,20 @@
-import { API_PATH_TYPE_COUNT } from "@/api/paths.ts";
+import { API_PATH_ALL_FACET_LIST, API_PATH_TYPE_COUNT } from "@/api/paths.ts";
 import { parseBaseParams } from "@/fetch/utils/parseBaseSearchParams.ts";
 import type { CountTypesRequestParams, CountTypesResponse } from "@/api/count/types.ts";
+import type { AllFacetListRequestParam } from "@/api/facets/all.ts";
 import type { AllSearchParams } from "@/schema/search/all.ts";
 import type { BaseSearchParams } from "@/schema/search/base.ts";
 
 export const fetchTypeCount = async (params: BaseSearchParams) => {
   const searchParams = parseParams(params);
-  const response = await fetch(`${API_PATH_TYPE_COUNT}?${new URLSearchParams(searchParams)}`, {
+  const response = await fetch(`${API_PATH_ALL_FACET_LIST}?${new URLSearchParams(searchParams)}`, {
     method: "GET",
   });
   const data = (await response.json()) as CountTypesResponse;
   return data;
 };
 
-const parseParams = (params: AllSearchParams): CountTypesRequestParams => {
+const parseParams = (params: AllSearchParams): AllFacetListRequestParam => {
   const base = parseBaseParams(params);
   delete base.page;
   delete base.perPage;
