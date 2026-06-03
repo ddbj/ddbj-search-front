b8001d25402bee984cafd00f1184e1a2f36122dd
satoshionoda <satoshionoda@yohak.design>
2026-06-03 13:10:36 +0900

    Update "About DDBJ Search" link and hide "Status" link in GlobalHeader

--- src/features/shared/GlobalHeader.tsx
+++ src/features/shared/GlobalHeader.tsx
@@ -14,8 +14,13 @@ export const GlobalHeader: FC<Props> = ({ breadcrumbsPaths }) => {
     <div className={"flex justify-between"}>
       <Breadcrumbs paths={breadcrumbsPaths} />
       <nav className={"flex gap-x-4"}>
-        <IconTextLink label={"About DDBJ Search"} to={"/about/"} Icon={InfoIcon} />
-        <IconTextLink label={"Status"} to={"/status/"} Icon={StatusIcon} />
+        <IconTextLink
+          label={"About DDBJ Search"}
+          to={"https://www.ddbj.nig.ac.jp/services/ddbj-search-e.html"}
+          Icon={InfoIcon}
+          target={"_blank"}
+        />
+        {/*<IconTextLink label={"Status"} to={"/status/"} Icon={StatusIcon} />*/}
         <IconTextLink label={"API"} to={"/api-doc/"} Icon={ApiIcon} target={"_blank"} />
       </nav>
     </div>
