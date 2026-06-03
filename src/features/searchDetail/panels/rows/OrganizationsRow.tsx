245bc509f0e5cd8bc9b36721b4761be92db88c98
satoshionoda <satoshionoda@yohak.design>
2026-06-03 12:54:52 +0900

    Refactor `OrganizationsRow` story to improve test cases and extract `OrganizationItem` component

--- src/features/searchDetail/panels/rows/OrganizationsRow.tsx
+++ src/features/searchDetail/panels/rows/OrganizationsRow.tsx
@@ -10,30 +10,31 @@ export const OrganizationsRow: FC<Props> = ({ organizations }) => {
     <InfoListItem term={detailFieldLabels.organizations}>
       <ul className={"flex flex-col gap-y-2"}>
         {organizations.sort(sortOrganizations).map((org, index) => (
-          <li key={index} className={"flex flex-col"}>
-            <div className={"text-xs text-gray-500"}>
-              {org.role}
-              {org.organizationType && (
-                <span>
-                  {" / "}
-                  {org.organizationType}
-                </span>
-              )}
-            </div>
-            <div>
-              {org.url ? (
-                <a href={org.url} target={"_blank"} className={"text-link-primary"}>
-                  {org.name}
-                </a>
-              ) : (
-                <span>{org.name}</span>
-              )}
-              {org.abbreviation && <span> ({org.abbreviation})</span>}
-            </div>
-          </li>
+          <OrganizationItem org={org} key={index} />
         ))}
       </ul>
     </InfoListItem>
+  );
+};
+const OrganizationItem: FC<{ org: Organization }> = ({ org }) => {
+  const roleStr = org.role ? `Role: ${org.role}` : "";
+  const typeStr = org.organizationType ? `Type: ${org.organizationType}` : "";
+  const firstRow = [roleStr, typeStr].filter((str) => str !== "").join(" / ");
+
+  return (
+    <li className={"flex flex-col"}>
+      <div className={"text-xs text-gray-500"}>{firstRow}</div>
+      <div>
+        {org.url ? (
+          <a href={org.url} target={"_blank"} className={"text-link-primary"}>
+            {org.name}
+          </a>
+        ) : (
+          <span>{org.name}</span>
+        )}
+        {org.abbreviation && <span> ({org.abbreviation})</span>}
+      </div>
+    </li>
   );
 };
 const sortOrganizations = (a: Organization, b: Organization) => {
